53c3175510a88264e25eaa41b5bbb2691ebc576f
vlad-tish <vlad_kamch@mail.ru>
2023-07-30 14:44:02 +1200

    Заменил метод if на тернарный оператор в № 1, 2

--- hw3.js
+++ hw3.js
@@ -1,20 +1,14 @@
 // Задание 1
 let password = ('пароль');
 let reply = prompt('Введите пароль');
-if (reply.toLowerCase() === password) {
-    console.log('Пароль введен верно');
-} else {
-    console.log('Пароль введен неправильно');
-}
+reply.toLowerCase() === password ? console.log('Пароль введен верно'):
+console.log('Пароль введен неправильно');
 // 
 
 // Задание 2
 let c = (2)
-if (c>0 && c<10) {
-    console.log('Верно');
-} else {
-    console.log('Неверно');
-}
+c>0 && c<10 ? console.log('Верно'):
+console.log('Неверно');
 //
 
 // Задание 3
