af8aea686bb342f346854186cf395791f93ae6a7
vlad-tish <vlad_kamch@mail.ru>
2023-07-31 21:16:59 +1200

    new file hw4.js

--- hw4.js
+++ hw4.js
@@ -1,35 +1,40 @@
-// Задание 1
-let i = 4;
-do {
-    console.log('Привет!');
-    i--;
-} while (i>2);
-// 
+// // Задание 1
+// let i = 4;
+// do {
+//     console.log('Привет!');
+//     i--;
+// } while (i>2);
+// // 
 
-// Задание 2
-let a = 1;
-while(a<6) {
-    console.log(a);
-    a++;
-}
-//
+// // Задание 2
+// let a = 1;
+// while(a<6) {
+//     console.log(a);
+//     a++;
+// }
+// //
 
-// Задание 3
-let b = 7;
-do {
-    console.log(b);
-    b++;
-} while(b<28);
-//
+// // Задание 3
+// let b = 7;
+// do {
+//     console.log(b);
+//     b++;
+// } while(b<28);
+// //
 
-//Задание 4
-while(true) {
-    let msg = prompt('Поговори с собой');
-    if(msg === 'continue') {
-        continue;
-    } 
-    if(msg === 'continue') {
-        break;
-    }
+// //Задание 4
+// const obj = {
+//     'Коля': 200,
+//     'Вася': 300,
+//     'Петя': 400,
+// }
+// for (let worker in obj) {
+//     alert(`${worker} - зарплата ${obj[worker]}`)
+// }
+// //
+
+//Задание 5
+for (let n = 1000, num; n>50; n/2 ) {
+
 }
 //
